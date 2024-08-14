77983118f4c4352842c9a4e4559bb17b78908a92
*Luan Calazans* <138558778+Luanqmata@users.noreply.github.com>
2024-08-14 19:55:59 -0300

    Create Tabuada.cs

---
+++ Tabuada.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Do_Zero
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite um numero para ver a tabuada: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i <= 10; i++)
+        {
+            int conta = num1 * i;
+            Console.WriteLine(num1 + " x " + i + " = " + conta);
+        }
+    }
+}
