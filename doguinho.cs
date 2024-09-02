a7150b2e3f78b07ff8d854b1a0e07a3797956aea
*Luan Calazans* <138558778+Luanqmata@users.noreply.github.com>
2024-09-02 20:23:06 -0300

    Create doguinho.cs

---
+++ doguinho.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Imprimir no terminal o doguinho?
+            __  
+       (___()'`;
+       /,    /` 
+       \\'--\\  
+ */
+            string doguinho = @"
+            __  
+       (___()'`;
+       /,    /` 
+       \\'--\\
+";
+            Console.WriteLine(doguinho);
+        }
+    }
+}
